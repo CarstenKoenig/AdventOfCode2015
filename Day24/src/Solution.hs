1ce81f8fbb4ff9da8b538be910d0a67dd656ab9c
Carsten König <carsten@gettingsharper.de>
2017-12-21 07:43:14 +0100

    Day24 star 1

--- Day24/src/Solution.hs
+++ Day24/src/Solution.hs
@@ -1,8 +1,10 @@
 module Solution (readInput, part1, part2)  where
 
+import Control.Monad (guard)
 import Data.Function (on)
 import Data.List (groupBy, sortBy)
-
+import qualified Control.Monad.State.Strict as S
+import Debug.Trace
 
 type Input = [Package]
 
@@ -10,17 +12,24 @@ type Package = Int
 
 part1 :: Input -> Int
 part1 inp =
-  let tw = targetWeight inp
-      (best,_,_) = compartmentConfig $ pickGroups tw inp
+  let tw = targetWeight2 inp
+      (best,_,_,_) = compartmentConfig2 $ pickGroups2 tw (reverse inp)
   in quantumEntanglement best
 
 
-part2 :: Input -> ()
-part2 inp = ()
+part2 :: Input -> Int
+part2 inp =
+  let tw = targetWeight inp
+      (best,_,_) = compartmentConfig $ pickGroups tw (reverse inp)
+  in quantumEntanglement best
 
 
 targetWeight :: Input -> Int
 targetWeight inp = let total = sum inp in total `div` 3
+
+
+targetWeight2 :: Input -> Int
+targetWeight2 inp = let total = sum inp in total `div` 4
 
 
 quantumEntanglement :: [Int] -> Int
@@ -30,18 +39,48 @@ quantumEntanglement = product
 compartmentConfig :: [([Int],[Int],[Int])] -> ([Int], [Int], [Int])
 compartmentConfig =
   head . sortBy (compare `on` fstQE) .
-  head . groupBy ((==) `on` fstLen). sortBy (compare `on` fstLen)
+  head . groupBy ((==) `on` fstLen)
   where fstLen (xs,_,_) = length xs
         fstQE (xs,_,_) = quantumEntanglement xs
 
 
-pickGroups :: Int -> [Int] -> [([Int],[Int],[Int])]
-pickGroups tgt ws = go tgt ws
+compartmentConfig2 :: [([Int],[Int],[Int],[Int])] -> ([Int], [Int], [Int], [Int])
+compartmentConfig2 =
+  head . sortBy (compare `on` fstQE) .
+  head . groupBy ((==) `on` fstLen)
+  where fstLen (xs,_,_,_) = length xs
+        fstQE (xs,_,_,_) = quantumEntanglement xs
+
+
+pickGroups2 :: Int -> [Int] -> [([Int],[Int],[Int],[Int])]
+pickGroups2 tgt ws = concatMap (\ l -> go l tgt ws) [1..]
   where
-    go t xs = do
-      (grp1,xs')  <- pickGroup t xs
+    go smallest t xs = do
+      (grp1,xs')  <- pickGroupWithLen smallest t xs
+      (grp2,xs'') <- pickGroup t xs'
+      (grp3,grp4) <- pickGroup t xs'
+      return (grp1,grp2,grp3,grp4)
+
+
+pickGroups :: Int -> [Int] -> [([Int],[Int],[Int])]
+pickGroups tgt ws = concatMap (\ l -> go l tgt ws) [1..]
+  where
+    go smallest t xs = do
+      (grp1,xs')  <- pickGroupWithLen smallest t xs
       (grp2,grp3) <- pickGroup t xs'
       return (grp1,grp2,grp3)
+
+
+pickGroupWithLen :: Int -> Int -> [Int] -> [([Int], [Int])]
+pickGroupWithLen len tgt ws = go len tgt ws
+  where go 0 0 xs = return $ ([], xs)
+        go _ _ [] = []
+        go 0 _ (_:_) = []
+        go l t (x:xs)
+          | x > t = [ (y,x:ys) | (y,ys) <- go l t xs ]
+          | otherwise =
+              [ (x:ys, rem) | (ys,rem) <- go (l-1) (t-x) xs ]
+              ++ [ (y,x:ys) | (y,ys) <- go l t xs ]
 
 
 pickGroup :: Int -> [Int] -> [([Int], [Int])]
